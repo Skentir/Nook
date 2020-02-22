bd29e555de7be5b870a22659f36bb066d7d74fd6
Kirsten S <daniellesison19@gmail.com>
2020-02-22 19:47:25 +0800

    Finished landing page

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,24 +1,6 @@
-var modal = document.getElementById("sign-in-modal");
+$(document).ready(function(){
 
-// Get the button that opens the modal
-var btn = document.getElementById("sign-in");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+    $(".close").click(function(){
+        $("#get-started-modal").modal("hide");
+    });
+});
