01722522c28ae8709cc2a56e6d69d948b1d0cca1
Phantominum <robinjerome23@gmail.com>
2020-02-29 00:21:45 +0800

    fixed modals, added form validation, added page navigation to modal buttons

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,50 @@
 $(document).ready(function(){
 
-    $(".close").click(function(){
-        $("#get-started-modal").modal("hide");
-    });
+    $("#sign-in-btn").click(function(){
+        
+        //boolean 
+        var valid = true;
+
+        valid = checkifValid($("#user-id"),valid);
+        valid = checkifValid($("#user-pass"),valid);
+
+        if(valid == true){
+            location.href = "explore.html";
+        }
+    })
+
+    $("#register-btn").click(function(){
+        
+        //boolean 
+        var valid = true;
+
+        valid = checkifValid($("#reg-fname"),valid);
+        valid = checkifValid($("#reg-lname"),valid);
+        valid = checkifValid($("#reg-email"),valid);
+        valid = checkifValid($("#reg-idnum"),valid);
+        valid = checkifValid($("#reg-pass"),valid);
+        valid = checkifValid($(".custom-select"),valid);
+        
+
+        if(valid == true){
+            location.href = "explore.html";
+        }
+    })
+
+    function checkifValid(field,val){
+        var check = val;
+        if(field.val() ==""){
+            check = false;
+            field.css({"border":"1px solid red"})
+        }
+        if(field.attr("class") =="custom-select"){
+            if(field.val() == "Select"){
+                heck = false;
+                field.css({"border":"1px solid red"}) 
+            }
+        }
+
+        return check;
+    }
+    
 });
