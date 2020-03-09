c42799678a0dc3517d5b8f2b8874b010e6367c8a
Kirsten S <daniellesison19@gmail.com>
2020-03-09 17:09:39 +0800

    Fixed File Structure

--- app.js
+++ app.js
@@ -25,51 +25,55 @@ app.use('/admin',adminRoute);
 //ROUTES
     
 app.get('/',(req,res) =>{
-    res.sendFile(path.join(__dirname + "/html/landing.html")); 
+    res.sendFile(path.join(__dirname + "/layouts/views/landing.html")); 
+})
+
+app.get('/landing',(req,res) =>{
+    res.sendFile(path.join(__dirname + "/layouts/views/landing.html")); 
 })
 
 app.get('/ad-eventreg', (req,res)=>{
-    res.sendFile(path.join(__dirname + "/html/ad-eventreg.html"));
+    res.sendFile(path.join(__dirname + "/layouts/views/ad-eventreg.html"));
 })
 
 app.get('/explore', (req,res)=>{
-    res.sendFile(path.join(__dirname + "/html/explore.html"));
+    res.sendFile(path.join(__dirname + "/layouts/views/explore.html"));
 })
 
 app.get('/ad-tools', (req,res)=>{
-    res.sendFile(path.join(__dirname + "/html/ad-tools.html"));
+    res.sendFile(path.join(__dirname + "/layouts/views/ad-tools.html"));
 })
 
 app.get('/edit-profile', (req,res)=>{
-    res.sendFile(path.join(__dirname + "/html/edit-profile.html"));
+    res.sendFile(path.join(__dirname + "/layouts/views/edit-profile.html"));
 })
 
 app.get('/edit-org', (req,res)=>{
-    res.sendFile(path.join(__dirname + "/html/edit-org.html"));
+    res.sendFile(path.join(__dirname + "/layouts/views/edit-org.html"));
 })
 
 app.get('/member-requests', (req,res)=>{
-    res.sendFile(path.join(__dirname + "/html/member-requests.html"));
+    res.sendFile(path.join(__dirname + "/layouts/views/member-requests.html"));
 })
 
 app.get('/planner', (req,res)=>{
-    res.sendFile(path.join(__dirname + "/html/planner.html"));
+    res.sendFile(path.join(__dirname + "/layouts/views/planner.html"));
 })
 
 app.get('/user-profile', (req,res)=>{
-    res.sendFile(path.join(__dirname + "/html/user-profile.html"));
+    res.sendFile(path.join(__dirname + "/layouts/views/user-profile.html"));
 })
 
 app.get('/viewevent', (req,res)=>{
-    res.sendFile(path.join(__dirname + "/html/viewevent.html"));
+    res.sendFile(path.join(__dirname + "/layouts/views/viewevent.html"));
 })
 
 app.get('/vieworg', (req,res)=>{
-    res.sendFile(path.join(__dirname + "/html/vieworg.html"));
+    res.sendFile(path.join(__dirname + "/layouts/views/vieworg.html"));
 })
 
 app.get('/view-officers', (req,res)=>{
-    res.sendFile(path.join(__dirname + "/html/view-officers.html"));
+    res.sendFile(path.join(__dirname + "/layouts/views/view-officers.html"));
 })
 
 
