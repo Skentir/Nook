a8ea7bf6bc65dcf23d7f2b88484886eec61a259d
Phantominum <robinjerome23@gmail.com>
2020-05-02 21:42:43 +0800

    fixed async for one image render

--- controllers/exploreController.js
+++ controllers/exploreController.js
@@ -43,39 +43,54 @@ exports.view = function(req, res) {
         async.parallel({
             //for each(for eaach) org queried, query filename and chunks(waterfall)
           orgs:function gatherOrgData(callback) {
-              OrgModel.find({}).select('_id org_type org_logo org_name').limit(2).then(results=>{
+              OrgModel.find({}).select('_id org_type org_logo org_name').limit(1).then(results=>{
                 if (results) {
-                    async.eachSeries(results, 
-                    function(org, callbackEach) {
-                        console.log("ok so this is the org---> " + org);
-                        console.log(callbackEach);
-                        callbackEach(org,callbackEach);
+                    async.waterfall([ 
+                    function(callbackEach) {
+                        //console.log("ok so this is the org---> " + org);
+                        //console.log(callbackEach);
+                        callbackEach(null, results[0]);
                     },
                     function getImageFilname(org, callbackEach){
-                        console.log("we are getting the filename for this org--->"+ org);
-                        var filename = collection.find({filename:org.org_logo}).toArray();
-                        console.log("2->>>"+callbackEach);
-                        callbackEach(filename,callbackEach);
+                        collection.find({filename: org.org_logo}).toArray(function(err, docs){
+                            if(err){
+                            return callbackEach(err);
+                            }
+                            if(!docs || docs.length === 0){
+                            return callbackEach(err);
+                            }else{
+                            //Retrieving the chunks from the db
+                                collectionChunks.find({files_id : docs[0]._id}).sort({n: 1}).toArray(function(err, chunks){
+                                    if(err){
+                                    return callbackEach(err);
+                                    }
+                                    if(!chunks || chunks.length === 0){
+                                    //No data found
+                                    return callbackEach(err);
+                                    }
+                                    //Append Chunks
+                                    var fileData = [];
+                                    for(let i=0; i<chunks.length;i++){
+                    
+                                    //This is in Binary JSON or BSON format, which is stored
+                                    //in fileData array in base64 endocoded string format
+                                    fileData.push(chunks[i].data.toString('base64'));
+                                    }
+                                    //Display the chunks using the data URI format
+                                    finalFile = 'data:' + docs[0].contentType + ';base64,' + fileData.join('');
+                                    images.push(finalFile);
+                                    callbackEach(null);
+                                });
+                            }
+                        })
+                        
                     },
-                    function getImageChunks(filename,callbackEach3){
-                        var imagechunks = collectionChunks.find({files_id : filename._id}).sort({n: 1}).toArray();
-                        callbackEach3(imagechunks,callbackEach3);
-                    },
-                    function formatAndAddImage(imagechunks, callbackEach){
-                        var fileData = [];
-                        for(let i=0; i<imagechunks.length;i++){
-                            fileData.push(imagechunks[i].data.toString('base64'));
-                        }
-
-                        finalFile = 'data:' + docs[0].contentType + ';base64,' + fileData.join('');
-                        images.push(finalFile);
-                        callbackEach(null);
-                    },
-                    function() {
+                    function(callbackEach) {
                     //when all item.somethingElse is done, call the upper callback
                         console.log(""+ images.length);
                         callback(null, results);
-                    });
+                    }
+                ]);
                 }
             })
           },
@@ -89,11 +104,11 @@ exports.view = function(req, res) {
           
           //combine results and render
    
-       }, function (err, docs) {
+       }, function (err, results) {
            if (err) {
              console.log("error")
          } else {
-             console.log(docs);
+             console.log(results.events);
          }
        })
 
