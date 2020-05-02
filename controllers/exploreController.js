803bebc51da51dec30a0b3a009b3cd0eeccd1b35
Phantominum <robinjerome23@gmail.com>
2020-05-02 20:29:01 +0800

    added async

--- controllers/exploreController.js
+++ controllers/exploreController.js
@@ -1,6 +1,11 @@
 const OrgModel = require('../models/Org');
 const EventModel = require('../models/Event');
 const async = require('async');
+const getDb = require('../config/db').getDb;
+const db = getDb();
+const collection = db.collection('uploads.files');
+const collectionChunks = db.collection('uploads.chunks');
+let images = [];
 exports.view = function(req, res) {
     /*OrgModel.find({})
         .select('_id org_type org_logo org_name')
@@ -40,11 +45,35 @@ exports.view = function(req, res) {
           orgs:function gatherOrgData(callback) {
               OrgModel.find({}).select('_id org_type org_logo org_name').limit(2).then(results=>{
                 if (results) {
-                    async.eachSeries(results, function(org, callbackEach) {
+                    async.eachSeries(results, 
+                    function(org, callbackEach) {
                         console.log("ok so this is the org---> " + org);
+                        console.log(callbackEach);
+                        callbackEach(org,callbackEach);
+                    },
+                    function getImageFilname(org, callbackEach){
+                        console.log("we are getting the filename for this org--->"+ org);
+                        var filename = collection.find({filename:org.org_logo}).toArray();
+                        console.log("2->>>"+callbackEach);
+                        callbackEach(filename,callbackEach);
+                    },
+                    function getImageChunks(filename,callbackEach3){
+                        var imagechunks = collectionChunks.find({files_id : filename._id}).sort({n: 1}).toArray();
+                        callbackEach3(imagechunks,callbackEach3);
+                    },
+                    function formatAndAddImage(imagechunks, callbackEach){
+                        var fileData = [];
+                        for(let i=0; i<imagechunks.length;i++){
+                            fileData.push(imagechunks[i].data.toString('base64'));
+                        }
+
+                        finalFile = 'data:' + docs[0].contentType + ';base64,' + fileData.join('');
+                        images.push(finalFile);
                         callbackEach(null);
-                    }, function() {
+                    },
+                    function() {
                     //when all item.somethingElse is done, call the upper callback
+                        console.log(""+ images.length);
                         callback(null, results);
                     });
                 }
--- services/getImage.js
+++ services/getImage.js
@@ -3,44 +3,39 @@ const getDb = require('../config/db').getDb;
 const db = getDb();
 const collection = db.collection('uploads.files');
 const collectionChunks = db.collection('uploads.chunks');
-function getImageLogo(results){
-    var imageArray =[];
-    for(var j = 0; j< results.length; j++){
-        collection.find({filename: results[j].org_logo}).toArray(function(err, docs){
+function getImageLogo(result,callback){
+    var finalFile;
+    collection.find({filename: result.org_logo}).toArray(function(err, docs){
+        if(err){
+        return callback(err);
+        }
+        if(!docs || docs.length === 0){
+        return callback(err);
+        }else{
+        //Retrieving the chunks from the db
+        collectionChunks.find({files_id : docs[0]._id}).sort({n: 1}).toArray(function(err, chunks){
             if(err){
-            return null;
+            return callback(err);
             }
-            if(!docs || docs.length === 0){
-            return null;
-            }else{
-            //Retrieving the chunks from the db
-            
-            collectionChunks.find({files_id : docs[0]._id}).sort({n: 1}).toArray(function(err, chunks){
-                if(err){
-                return null;
-                }
-                if(!chunks || chunks.length === 0){
-                //No data found
-                return null;
-                }
-                //Append Chunks
-                var fileData = [];
-                for(let i=0; i<chunks.length;i++){
+            if(!chunks || chunks.length === 0){
+            //No data found
+            return callback(err);
+            }
+             //Append Chunks
+            var fileData = [];
+            for(let i=0; i<chunks.length;i++){
 
-                //This is in Binary JSON or BSON format, which is stored
-                //in fileData array in base64 endocoded string format
-                fileData.push(chunks[i].data.toString('base64'));
-                }
-                //Display the chunks using the data URI format
-                var finalFile = 'data:' + docs[0].contentType + ';base64,' + fileData.join('');
-                imageArray.push(finalFile);
-                console.log(""+imageArray.length);
-            });
+            //This is in Binary JSON or BSON format, which is stored
+            //in fileData array in base64 endocoded string format
+            fileData.push(chunks[i].data.toString('base64'));
             }
-        })
-    }
-   
-    return imageArray;
+            //Display the chunks using the data URI format
+            finalFile = 'data:' + docs[0].contentType + ';base64,' + fileData.join('');
+            arrays.push(finalFile);
+        });
+        }
+    })
+    return callback(null,finalFile);
 
 }
 
