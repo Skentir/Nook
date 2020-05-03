fff120af50c6f9e6fc5edb15a1ae4097376b56fc
Kirsten S <daniellesison19@gmail.com>
2020-05-03 18:57:46 +0800

    Added org image rendering

--- controllers/orgsController.js
+++ controllers/orgsController.js
@@ -1,5 +1,10 @@
 const User = require('../models/User');
 const OrgModel = require('../models/Org');
+const async = require('async');
+const getDb = require('../config/db').getDb;
+const db = getDb();
+const collection = db.collection('uploads.files');
+const collectionChunks = db.collection('uploads.chunks');
 
 exports.editorg = (req,res)=> {
     var orgId = req.params.orgId;
@@ -62,7 +67,7 @@ exports.viewofficers = (req,res)=> {
 
 exports.vieworg = (req,res) => {
     const orgId = req.params.orgId;
-
+    /*
     OrgModel.findById(orgId)
         .populate('events', '_id event_name header_photo')
         .exec(function(err, result)  {
@@ -80,4 +85,112 @@ exports.vieworg = (req,res) => {
                 res.render('vieworg', params);       
             }
         });
+    */
+   var finalFile1, eventList = [], org;
+   OrgModel.findById(orgId)
+   .populate('events', '_id event_name header_photo')
+       .exec(function(err, result) {
+           if (err) {
+               res.send(err);
+           } else if (!result) {
+               // Org not Found
+               res.redirect('/explore');
+           } else { 
+               collection.find({filename: result.org_header})
+                   .toArray(function(err, docs) {
+                       if(err) {
+                        return res.send(err);
+                       }
+                       if(!docs || docs.length === 0){
+                        return res.send(err);
+                       } else {
+                       //Retrieving the chunks from the db
+                       collectionChunks.find({files_id : docs[0]._id}).sort({n: 1})
+                           .toArray(function(err, chunks) {
+                               if(err){
+                                return res.send(err);
+                               }
+                               if(!chunks || chunks.length === 0){
+                               //No data found
+                                return res.send(err);
+                               }
+                               //Append Chunks
+                               var fileData = [];
+                               for(let i=0; i<chunks.length;i++){
+                   
+                               //This is in Binary JSON or BSON format, which is stored
+                               //in fileData array in base64 endocoded string format
+                               fileData.push(chunks[i].data.toString('base64'));
+                               }
+                               //Display the chunks using the data URI format
+                               finalFile1 = 'data:' + docs[0].contentType + ';base64,' + fileData.join('');
+                               org = JSON.parse(JSON.stringify(result));
+                               org.orgImage = finalFile1;
+                               console.log("HELLO " + result.events)
+                                    async.forEach(result.events, function(result,resultCallback) {
+                                        async.waterfall([ 
+                                            function(callbackEach) {
+                                                callbackEach(null, result);
+                                            },
+                                            function getImageFilname(event, callbackEach){
+                                                collection.find({filename: event.header_photo}).toArray(function(err, docs){
+                                                    if(err){
+                                                    return callbackEach(err);
+                                                    }
+                                                    if(!docs || docs.length === 0){
+                                                    return callbackEach(err);
+                                                    }else{
+                                                    //Retrieving the chunks from the db
+                                                        collectionChunks.find({files_id : docs[0]._id}).sort({n: 1}).toArray(function(err, chunks){
+                                                            if(err){
+                                                            return callbackEach(err);
+                                                            }
+                                                            if(!chunks || chunks.length === 0){
+                                                            //No data found
+                                                            return callbackEach(err);
+                                                            }
+                                                            //Append Chunks
+                                                            var fileData = [];
+                                                            for(let i=0; i<chunks.length;i++){
+                                            
+                                                            //This is in Binary JSON or BSON format, which is stored
+                                                            //in fileData array in base64 endocoded string format
+                                                            fileData.push(chunks[i].data.toString('base64'));
+                                                            }
+                                                            //Display the chunks using the data URI format
+                                                            finalFile = 'data:' + docs[0].contentType + ';base64,' + fileData.join('');
+                        
+                                                            //create a json object for the org
+                                                            var eventj = JSON.parse(JSON.stringify(event));
+                        
+                                                            //add the image property to json object and assign the image uri
+                                                            eventj.img = finalFile;
+                        
+                                                            //push it into list of orgs
+                                                            eventList.push(eventj);
+                                                            callbackEach(null);
+                                                        });
+                                                    }
+                                                })
+                                                
+                                            },
+                                                function(callbackEach) {  
+                                                    resultCallback();         
+                                                }
+                                        ])
+                                    }, function(err) { 
+                                            var params = {
+                                                layout: 'main',
+                                                org: org,
+                                                events: eventList
+                                            };
+                                            // console.log(params);
+                                           // res.send(params);
+                                            res.render('vieworg',params);
+                                    })
+                            })
+                        }
+                   }) 
+               }
+       })
 };
