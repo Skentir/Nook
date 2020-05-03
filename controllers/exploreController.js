c91cedc14e412ea9decd064c99dd409962c253f7
Phantominum <robinjerome23@gmail.com>
2020-05-03 22:40:51 +0800

    added rendering search

--- controllers/exploreController.js
+++ controllers/exploreController.js
@@ -157,9 +157,81 @@ exports.view = function(req, res) {
 
 exports.searchOrg = function(req, res) {
     var org_name = req.query.org_name;
-   
+    var orgList = [];
     OrgModel.find({'org_name': { "$regex": org_name, "$options": "i" }})
-    .select('_id org_name org_logo')
+    .select('_id org_name org_logo').then(results=>{
+        if (results) {
+            async.forEach(results, function(result,resultCallback){
+            async.waterfall([ 
+            function(callbackEach) {
+                callbackEach(null, result);
+            },
+            function getImageFilname(org, callbackEach){
+                collection.find({filename: org.org_logo}).toArray(function(err, docs){
+                    if(err){
+                    return callbackEach(err);
+                    }
+                    if(!docs || docs.length === 0){
+                    return callbackEach(err);
+                    }else{
+                    //Retrieving the chunks from the db
+                        collectionChunks.find({files_id : docs[0]._id}).sort({n: 1}).toArray(function(err, chunks){
+                            if(err){
+                            return callbackEach(err);
+                            }
+                            if(!chunks || chunks.length === 0){
+                            //No data found
+                            return callbackEach(err);
+                            }
+                            //Append Chunks
+                            var fileData = [];
+                            for(let i=0; i<chunks.length;i++){
+            
+                            //This is in Binary JSON or BSON format, which is stored
+                            //in fileData array in base64 endocoded string format
+                            fileData.push(chunks[i].data.toString('base64'));
+                            }
+                            //Display the chunks using the data URI format
+                            var finalFile = 'data:' + docs[0].contentType + ';base64,' + fileData.join('');
+    
+                            //create a json object for the org
+                            var orgj = JSON.parse(JSON.stringify(org));
+    
+                            //add the image property to json object and assign the image uri
+                            orgj.img = finalFile;
+    
+                            //push it into list of orgs
+                            orgList.push(orgj);
+                            callbackEach(null);
+                        });
+                    }
+                })
+                
+            },
+                function(callbackEach) {
+                    resultCallback();
+                    //callback(null, results);
+                }
+            ]);},function(err){
+                if(err){
+                    res.send(err);
+                }
+                else {
+                    var params = {
+                        layout: 'main',
+                        orgs: orgList,
+                    };
+                    res.render('search', params);
+                }
+            })
+        }
+    
+    })
+    
+    
+    
+    
+    /*
     .exec( function(err, docs){
         if (err) {
             res.send(err);
@@ -171,5 +243,5 @@ exports.searchOrg = function(req, res) {
             };
             res.render('search', params);
         }
-    });
+    });*/
 }
