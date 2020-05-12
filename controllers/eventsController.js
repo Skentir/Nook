f7d54e6b359a25cabfe9f05cdca8c8815e0dc381
Phantominum <robinjerome23@gmail.com>
2020-05-13 00:32:45 +0800

    added update event and org details, added create event

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -5,13 +5,50 @@ const db = getDb();
 const collection = db.collection('uploads.files');
 const collectionChunks = db.collection('uploads.chunks');
 
+var orgID;
+
 exports.addevent = (req,res)=>{
-    res.render('ad-eventreg');
+    var orgId = req.params.orgId;
+    var params = {
+        orgId : orgId
+    }
+    res.render('ad-eventreg',params);
 };
+
+exports.createevent = (req, res) =>{
+   var orgId =  req.params.orgId;
+    if(!req.file){
+        var event_photo = "117711a9ff985c8287cbdf2504cb1a51.jpg";
+    }
+    else{
+        event_photo = req.file.filename;
+    }
+    const event = new EventModel({
+        event_name: req.body.event_name,
+        about_desc: req.body.about_event,
+        things: req.body.things, 
+        capacity: req.body.capacity,
+        header_photo: event_photo,
+        venue: req.body.event_venue,
+        date: req.body.event_date,  
+        start_time:req.body.start_time,
+        end_time:req.body.end_time,
+        incentives: req.body.incentives,
+        codes: req.body.coursecodes,
+        organizer_id: req.params.orgId
+    })
+    event.save().then(event =>{
+        res.redirect('/admin/ad-tools');
+    })
+}
+
+
+
+
 
 exports.viewevents = (req,res)=>{
     var orgId = req.params.orgId;
-
+    orgID = req.params.orgId;
     EventModel.find({'organizer_id':orgId}, {event_name:1, date:1})
         .exec(function(err, result) {
             if (err) {
@@ -37,14 +74,45 @@ exports.editevent = (req,res)=> {
             } else if (!result) {
                 // No event found
                 res.redirect('/ad-tools');
-            } else {
-                event = JSON.parse(JSON.stringify(result));
-                var params = {
-                    layout: 'main',
-                    event
-                };
-                res.render('editevent', params);
-            }
+            } else {    
+                 collection.find({filename: result.header_photo }).toArray(function(err, docs){
+                if(err){
+                  return res.send(err);
+                }
+                if(!docs || docs.length === 0){
+                  return res.send(err);
+                }else{
+                  //Retrieving the chunks from the db
+                  
+                  collectionChunks.find({files_id : docs[0]._id}).sort({n: 1}).toArray(function(err, chunks){
+                    if(err){
+                      return res.send(err);
+                    }
+                    if(!chunks || chunks.length === 0){
+                      //No data found
+                      return res.send(err);
+                    }
+                    //Append Chunks
+                    var fileData = [];
+                    for(let i=0; i<chunks.length;i++){
+        
+                      //This is in Binary JSON or BSON format, which is stored
+                      //in fileData array in base64 endocoded string format
+                      fileData.push(chunks[i].data.toString('base64'));
+                    }
+                    //Display the chunks using the data URI format
+                    var finalFile = 'data:' + docs[0].contentType + ';base64,' + fileData.join('');
+                    var event = JSON.parse(JSON.stringify(result));
+                    var params = {
+                        layout: 'main',
+                        event,
+                        image: finalFile,
+                    }
+                    res.render('editevent', params);  
+                  });
+                }
+            } 
+        )}
         });
 };
 
@@ -136,3 +204,57 @@ exports.viewevent = (req,res)=> {
                 }
         })
 };
+
+
+
+exports.editeventdetails = (req, res) =>{
+    if(!req.file){
+    EventModel.findByIdAndUpdate({_id: req.params.eventId}, 
+      {
+      $set: {
+        event_name: req.body.edit_event_name,
+        about_desc: req.body.edit_about_desc,
+        things: req.body.edit_things, 
+        venue: req.body.edit_venue,
+        date: req.body.edit_event_date,  
+        start_time:req.body.edit_start_time,
+        end_time:req.body.edit_end_time,
+        incentives: req.body.edit_event_incentives
+
+      }
+      }, (err)=>{
+        if(err){
+
+          res.send(err);
+        }
+        else{
+          res.redirect('/admin/eventlist/'+ orgID);
+        }
+      })
+    }
+
+   else{
+    EventModel.findByIdAndUpdate({_id: req.params.eventId}, 
+      {
+        $set: {
+            event_name: req.body.edit_event_name,
+            about_desc: req.body.edit_about_desc,
+            things: req.body.edit_things, 
+            header_photo: req.file.filename,
+            venue: req.body.edit_venue,
+            date: req.body.edit_event_date,  
+            start_time:req.body.edit_start_time,
+            end_time:req.body.edit_end_time,
+            incentives: req.body.edit_event_incentives
+      }
+      }, (err)=>{
+        if(err){
+
+          res.send(err);
+        }
+        else{
+          res.redirect('/admin/eventlist/'+ orgID);
+        }
+      })
+    }
+}
--- routes/admin.js
+++ routes/admin.js
@@ -5,9 +5,53 @@ const eventsController = require('../controllers/eventsController');
 const orgsController = require('../controllers/orgsController');
 const userController = require('../controllers/userController');
 const requestsController = require('../controllers/requestsController');
+const multer = require('multer');
+const crypto = require("crypto");
+const GridFsStorage = require("multer-gridfs-storage");
+const bodyParser = require('body-parser');
+const Grid = require('gridfs-stream');
+const mongoose = require('mongoose');
+const path = require('path');
+const initDb = require("../config/db").initDb;
+const getDb = require("../config/db").getDb;
+
+let gfs; 
+
+var db = getDb();
+
+
+db.once('open', ()=>{
+    //Init Stream
+    gfs = Grid(db, mongoose.mongo);
+    gfs.collection('uploads');
+})
+
+//create storage engine for user and event reg
+const storage = new GridFsStorage({
+  db: db,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+        //crypto to generate random names
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        //create filename
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+
+const upload = multer({storage: storage});
 
 router.get('/ad-tools', userController.viewtools);
-router.get('/ad-eventreg', eventsController.addevent);
+router.get('/ad-eventreg/:orgId', eventsController.addevent);
 router.get('/eventlist/:orgId', eventsController.viewevents);
 router.get('/editorg/:orgId', orgsController.editorg);
 router.get('/editevent/:eventId', eventsController.editevent);
@@ -15,5 +59,7 @@ router.get('/member-requests/:orgId', requestsController.viewrequests);
 router.get('/view-officers/:orgId', orgsController.viewofficers);
 router.delete('/delete-request/:reqId', requestsController.deleterequest);
 router.put('/accept-request/:reqId', requestsController.acceptrequest);
-
+router.post('/editorg/:orgId',upload.single('edit_org_header'), orgsController.editorgdetails);
+router.post('/editevent/:eventId', upload.single('edit_event_photo'), eventsController.editeventdetails);
+router.post('/ad-eventreg/:orgId', upload.single('event_photo'),eventsController.createevent);
 module.exports = router;
