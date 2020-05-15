54986743c6c8ec7acac094f61b2dd9f10da39fb2
Kirsten S <daniellesison19@gmail.com>
2020-05-15 11:28:17 +0800

    Added my organizations rendering

--- controllers/userController.js
+++ controllers/userController.js
@@ -326,6 +326,7 @@ exports.renderUser = (req, res) => {
         } else {
         var fileName = res.locals.photo;
         var userId = req.session.passport.user;
+        var orgList = [], user;
         User.findById(userId)
                 .populate('orgs.org_id','_id org_name org_logo')
                 .exec( function(err,result) { 
@@ -359,14 +360,69 @@ exports.renderUser = (req, res) => {
                                 }
                                 //Display the chunks using the data URI format
                                 var finalFile = 'data:' + docs[0].contentType + ';base64,' + fileData.join('');
-                                var user = JSON.parse(JSON.stringify(result));
-                                var params = {
-                                    layout: 'main',
-                                    isUser: true,
-                                    info:user,
-                                    image: finalFile
-                                }
-                                res.render('user-profile', params);  
+                                user = JSON.parse(JSON.stringify(result));
+                                user.img = finalFile;
+
+                                    async.forEach(result.orgs, function(result,resultCallback) {
+                                      async.waterfall([ 
+                                          function(callbackEach) {
+                                              callbackEach(null, result);
+                                          },
+                                          function getImageFilname(org, callbackEach){
+                                              collection.find({filename: org.org_id.org_logo}).toArray(function(err, docs){
+                                                  if(err){
+                                                  return callbackEach(err);
+                                                  }
+                                                  if(!docs || docs.length === 0){
+                                                  return callbackEach(err);
+                                                  }else{
+                                                  //Retrieving the chunks from the db
+                                                      collectionChunks.find({files_id : docs[0]._id}).sort({n: 1}).toArray(function(err, chunks){
+                                                          if(err){
+                                                          return callbackEach(err);
+                                                          }
+                                                          if(!chunks || chunks.length === 0){
+                                                          //No data found
+                                                          return callbackEach(err);
+                                                          }
+                                                          //Append Chunks
+                                                          var fileData = [];
+                                                          for(let i=0; i<chunks.length;i++){
+                                          
+                                                          //This is in Binary JSON or BSON format, which is stored
+                                                          //in fileData array in base64 endocoded string format
+                                                          fileData.push(chunks[i].data.toString('base64'));
+                                                          }
+                                                          //Display the chunks using the data URI format
+                                                          finalFile = 'data:' + docs[0].contentType + ';base64,' + fileData.join('');
+                      
+                                                          //create a json object for the org
+                                                          var orgj = JSON.parse(JSON.stringify(org));
+                      
+                                                          //add the image property to json object and assign the image uri
+                                                          orgj.img = finalFile;
+                      
+                                                          //push it into list of orgs
+                                                          orgList.push(orgj);
+                                                          callbackEach(null);
+                                                      });
+                                                  }
+                                              })
+                                              
+                                          },
+                                              function(callbackEach) {  
+                                                  resultCallback();         
+                                              }
+                                      ])
+                                  }, function(err) { 
+                                          var params = {
+                                              layout: 'main',
+                                              isUser: true,
+                                              info: user,
+                                              orgs: orgList
+                                          };
+                                        res.render('user-profile',params);
+                                  })
                               });
                             }
                         } 
